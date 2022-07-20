45e1068f836e853c6b6c3dc5c7786e038e9aad02
amrusiy <amrusi.yarin@gmail.com>
2022-07-21 00:16:40 +0300

    update item with buttons and backgroud image

--- src/content/content.js
+++ src/content/content.js
@@ -2,7 +2,7 @@ export const data = [
   {
     categoryName: 'beers',
     items: [
-      { name: 'goldstar', img: '#', price: 15 },
+      { name: 'goldstar', img: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b1/Goldstar_logo.svg/220px-Goldstar_logo.svg.png', price: 15 },
       { name: 'carlsberg', img: '#', price: 15 },
       { name: 'corona', img: '#', price: 15 },
     ],
@@ -13,6 +13,20 @@ export const data = [
       { name: 'arak', img: '#', price: 5 },
       { name: 'rum', img: '#', price: 5 },
       { name: 'whiskey', img: '#', price: 5 },
+    ],
+  },
+  {
+    categoryName: 'soft drinks',
+    items: [
+      { name: 'cola', img: '#', price: 5 },
+      { name: 'ziro', img: '#', price: 5 },
+      { name: 'sprite', img: '#', price: 5 },
+      { name: 'cola', img: '#', price: 5 },
+      { name: 'ziro', img: '#', price: 5 },
+      { name: 'sprite', img: '#', price: 5 },
+      { name: 'cola', img: '#', price: 5 },
+      { name: 'ziro', img: '#', price: 5 },
+      { name: 'sprite', img: '#', price: 5 },
     ],
   },
 ];
