fcfc374c9154255023125f549ef71f5f304d3700
odedre <oded.reuveny@gmail.com>
2022-09-07 22:47:11 +0300

    shopping cart content

--- src/shopping-cart/shopping-cart.jsx
+++ src/shopping-cart/shopping-cart.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import styles from './shopping-cart.module.css';
-import {Item} from '../item';
-import { Counter } from '../counter';
-
-export function ShoppingCart() {
-    const price = Item.price;
-    const name = Item.name;
-    const amount = Counter.amount;
-    const line = {name}+" " + {price}+" "+ {amount} + " = " + String({amount}*{price});
-    return (
-        <div className={styles.shoppingCart}>
-            <h2>shopping cart</h2>
-            <div id="line"> {line} </div>
-            <div className={styles.orderLine}>
-                
-            </div>
-        </div>
-    )
+// import { Item } from '../item';
+// import { Counter } from '../counter';
+const order = {
+  drinks: [
+    { name: 'goldstar', price: 15, amount: 1  },
+    { name: 'carlsberg', price: 15, amount: 3  },
+    { name: 'arak', price: 15, amount: 5  },
+      
+  ]
 }
 
+export function ShoppingCart() {
+  console.log({ order });
+
+  return (
+    <div className={styles.shoppingCart}>
+      <h2>shopping cart</h2>
+      {order.map((item) => {
+        return (
+          <div>
+            <div>{item.name}</div>
+            <div className={styles.orderLine}>{item.amount}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
